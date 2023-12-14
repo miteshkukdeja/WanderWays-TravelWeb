1e1b76b424e3fb8cca395e72b91ceaecbfb8e503
miteshkukdeja <mrkukdeja006@gmail.com>
2023-12-14 18:40:06 +0530

    ✨feature: Update components layout, camp, features, guide

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel",
+  title: "Travel Camp",
   description: "Travel UI/UX App for Camping",
 };
 
